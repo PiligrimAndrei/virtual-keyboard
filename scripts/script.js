81cbccc8ac11755afe1fdc78945e826548791316
Andrei Krasko <andrei@krasko.by>
2022-05-05 13:27:59 +0300

    feat:  add render keyboard

---
+++ scripts/script.js
@@ -0,0 +1,524 @@
+const data = [
+   {
+      id: 1,
+      size: 1,
+      type: 'normal',
+      ru: ['ё', 'Ё'],
+      en: ['`', '~']
+   },
+
+   {
+      id: 2,
+      size: 1,
+      type: 'number',
+      ru: ['1', '!'],
+      en: ['1', '!']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['2', '"'],
+      en: ['2', '@']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['3', '№'],
+      en: ['3', '#']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['4', ';'],
+      en: ['4', '$']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['5', '%'],
+      en: ['5', '%']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['6', ':'],
+      en: ['6', '^']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['7', '?'],
+      en: ['7', '&']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['8', '*'],
+      en: ['8', '*']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['9', '('],
+      en: ['9', '(']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['0', ')'],
+      en: ['0', ')']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['-', '_'],
+      en: ['-', '_']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'number',
+      ru: ['=', '+'],
+      en: ['=', '+']
+   },
+   {
+      id: 3,
+      size: 3,
+      type: 'functional',
+      ru: ['Backspace', 'Backspace'],
+      en: ['Backspace', 'Backspace']
+   },
+   {
+      id: 3,
+      size: 2,
+      type: 'functional',
+      ru: ['Tab', 'Tab'],
+      en: ['Tab', 'Tab']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['DEL', 'DEL'],
+      en: ['DEL', 'DEL']
+   },
+   {
+      id: 3,
+      size: 3,
+      type: 'functional',
+      ru: ['Caps Lock', 'Caps Lock'],
+      en: ['Caps Lock', 'Caps Lock']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 3,
+      type: 'functional',
+      ru: ['ENTER', 'ENTER'],
+      en: ['ENTER', 'ENTER']
+   },
+   {
+      id: 3,
+      size: 3,
+      type: 'functional',
+      ru: ['SHIFT', 'SHIFT'],
+      en: ['SHIFT', 'SHIFT']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   }, {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'normal',
+      ru: ['й', 'Й'],
+      en: ['q', 'Q']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['^', '^'],
+      en: ['^', '^']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['Shift', 'Shift'],
+      en: ['Shift', 'Shift']
+   },
+   {
+      id: 3,
+      size: 2,
+      type: 'functional',
+      ru: ['CTRL', 'CTRL'],
+      en: ['CTRL', 'CTRL']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['WIN', 'WIN'],
+      en: ['WIN', 'WIN']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['ALT', 'ALT'],
+      en: ['ALT', 'ALT']
+   },
+
+   {
+      id: 3,
+      size: 9,
+      type: 'functional',
+      ru: [' ', ' '],
+      en: [' ', ' ']
+   },
+
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['ALT', 'ALT'],
+      en: ['ALT', 'ALT']
+   },
+   {
+      id: 3,
+      size: 2,
+      type: 'functional',
+      ru: ['CTRL', 'CTRL'],
+      en: ['CTRL', 'CTRL']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['<', '<'],
+      en: ['<', '<']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['/', '/'],
+      en: ['/', '/']
+   },
+   {
+      id: 3,
+      size: 1,
+      type: 'functional',
+      ru: ['>', '>'],
+      en: ['>', '>']
+   },
+
+]
+
+const renderKeyboardToDom = () => {
+   /*const keyboardWrapper = getKeyboardWrapper();*/
+   let keyboardWrapper = document.createElement('div');
+   keyboardWrapper.className = ('keyboard-wrapper')
+   let keyboard = document.createElement('div');
+   keyboard.className = ('keyboard')
+
+   keyboardWrapper.appendChild(keyboard);
+   document.body.appendChild(keyboardWrapper);
+   generateKeys(data).forEach(key => {
+      keyboard.append(key.generateKey())
+   })
+
+}
+
+const generateKeys = (data) => {
+   let keys = [];
+   data.forEach(elem => {
+      keys.push(new Key(elem))
+
+   });
+   console.log(data);
+   console.log(keys);
+   return keys
+
+}
+
+/* Class Key */
+class Key {
+   constructor({ id, size, type, ru, en }) {
+      this.id = id;
+      this.size = size;
+      this.type = type;
+      this.ru = ru;
+      this.en = en;
+   }
+   generateKey() {
+      let template = '';
+      /* TODO  add keyboard-wrapper & keyboard */
+
+      console.log('this', this)
+      let key = document.createElement('button');
+      key.className = 'key'
+      key.setAttribute('data-id', this.id)
+      key.classList.add('size' + this.size, this.type)
+      if (this.en) {
+         template += `<span class='eng'>`
+         template += `<span class='capsDown'> ${this.en[0]}</span>`
+         template += `<span class='capsUp hidden'> ${this.en[1]}</span>`
+         template += `<span class='capsOn hidden'> ${this.en[1]}</span>`
+         template += `<span class='shiftCapsOn hidden'> ${this.en[1]}</span>`
+         template += `</span>`
+      }
+      if (this.ru) {
+         template += `<span class='ru hidden'>`;
+         template += `<span class='capsDown hidden'> ${this.ru[0]}</span>`;
+         template += `<span class='capsUp hidden'> ${this.ru[1]}</span>`;
+         template += `<span class='capsOn hidden'> ${this.ru[1]}</span>`;
+         template += `<span class='shiftCapsOn hidden'> ${this.ru[1]}</span>`;
+         template += `</span>`;
+      }
+
+      key.innerHTML = template;
+      return key
+   }
+
+}
+
+console.log(renderKeyboardToDom())
+
+
