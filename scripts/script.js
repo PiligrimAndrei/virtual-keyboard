add79f9985bf62024a408d1f99a9e48ceff786b2
Andrei Krasko <andrei@krasko.by>
2022-05-09 13:47:55 +0300

    feat: final correct

--- scripts/script.js
+++ scripts/script.js
@@ -1,448 +1,68 @@
 const data = [
-   {
-      id: 'Backquote',
-      size: 1,
-      type: 'normal',
-      ru: ['ё', 'Ё'],
-      en: ['`', '~']
-   },
-
-   {
-      id: 'Digit1',
-      size: 1,
-      type: 'number',
-      ru: ['1', '!'],
-      en: ['1', '!']
-   },
-   {
-      id: 'Digit2',
-      size: 1,
-      type: 'number',
-      ru: ['2', '"'],
-      en: ['2', '@']
-   },
-   {
-      id: 'Digit3',
-      size: 1,
-      type: 'number',
-      ru: ['3', '№'],
-      en: ['3', '#']
-   },
-   {
-      id: 'Digit4',
-      size: 1,
-      type: 'number',
-      ru: ['4', ';'],
-      en: ['4', '$']
-   },
-   {
-      id: 'Digit5',
-      size: 1,
-      type: 'number',
-      ru: ['5', '%'],
-      en: ['5', '%']
-   },
-   {
-      id: 'Digit6',
-      size: 1,
-      type: 'number',
-      ru: ['6', ':'],
-      en: ['6', '^']
-   },
-   {
-      id: 'Digit7',
-      size: 1,
-      type: 'number',
-      ru: ['7', '?'],
-      en: ['7', '&']
-   },
-   {
-      id: 'Digit8',
-      size: 1,
-      type: 'number',
-      ru: ['8', '*'],
-      en: ['8', '*']
-   },
-   {
-      id: 'Digit9',
-      size: 1,
-      type: 'number',
-      ru: ['9', '('],
-      en: ['9', '(']
-   },
-   {
-      id: 'Digit0',
-      size: 1,
-      type: 'number',
-      ru: ['0', ')'],
-      en: ['0', ')']
-   },
-   {
-      id: 'Minus',
-      size: 1,
-      type: 'number',
-      ru: ['-', '_'],
-      en: ['-', '_']
-   },
-   {
-      id: 'Equal',
-      size: 1,
-      type: 'number',
-      ru: ['=', '+'],
-      en: ['=', '+']
-   },
-   {
-      id: 'Backspace',
-      size: 3,
-      type: 'functional',
-      ru: ['Backspace', 'Backspace'],
-      en: ['Backspace', 'Backspace']
-   },
-   {
-      id: 'Tab',
-      size: 2,
-      type: 'functional',
-      ru: ['Tab', 'Tab'],
-      en: ['Tab', 'Tab']
-   },
-   {
-      id: 'KeyQ',
-      size: 1,
-      type: 'normal',
-      ru: ['й', 'Й'],
-      en: ['q', 'Q']
-   },
-   {
-      id: 'KeyW',
-      size: 1,
-      type: 'normal',
-      ru: ['ц', 'Ц'],
-      en: ['w', 'W']
-   },
-   {
-      id: 'KeyE',
-      size: 1,
-      type: 'normal',
-      ru: ['у', 'У'],
-      en: ['e', 'E']
-   },
-   {
-      id: 'KeyR',
-      size: 1,
-      type: 'normal',
-      ru: ['к', 'К'],
-      en: ['r', 'R']
-   }, {
-      id: 'KeyT',
-      size: 1,
-      type: 'normal',
-      ru: ['е', 'Е'],
-      en: ['t', 'T']
-   },
-   {
-      id: 'KeyY',
-      size: 1,
-      type: 'normal',
-      ru: ['н', 'Н'],
-      en: ['y', 'Y']
-   }, {
-      id: 'KeyU',
-      size: 1,
-      type: 'normal',
-      ru: ['г', 'Г'],
-      en: ['u', 'U']
-   },
-   {
-      id: 'KeyI',
-      size: 1,
-      type: 'normal',
-      ru: ['ш', 'Ш'],
-      en: ['i', 'I']
-   }, {
-      id: 'KeyO',
-      size: 1,
-      type: 'normal',
-      ru: ['щ', 'Щ'],
-      en: ['o', 'O']
-   },
-   {
-      id: 'KeyP',
-      size: 1,
-      type: 'normal',
-      ru: ['з', 'З'],
-      en: ['p', 'P']
-   },
-   {
-      id: 'BracketLeft',
-      size: 1,
-      type: 'normal',
-      ru: ['х', 'Х'],
-      en: ['[', '{']
-   },
-   {
-      id: 'BracketRight',
-      size: 1,
-      type: 'normal',
-      ru: ['ъ', 'Ъ'],
-      en: [']', '}']
-   },
-   {
-      id: 'Backslash',
-      size: 1,
-      type: 'normal',
-      ru: ['&#92', '&#47;'],
-      en: ['&#92;', '|']
-   },
-   {
-      id: 'Delete',
-      size: 1,
-      type: 'functional',
-      ru: ['DEL', 'DEL'],
-      en: ['DEL', 'DEL']
-   },
-   {
-      id: 'CapsLock',
-      size: 3,
-      type: 'functional',
-      ru: ['Caps Lock', 'Caps Lock'],
-      en: ['Caps Lock', 'Caps Lock']
-   },
-   {
-      id: 'KeyA',
-      size: 1,
-      type: 'normal',
-      ru: ['ф', 'Ф'],
-      en: ['a', 'A']
-   },
-   {
-      id: 'KeyS',
-      size: 1,
-      type: 'normal',
-      ru: ['ы', 'Ы'],
-      en: ['s', 'S']
-   },
-   {
-      id: 'KeyD',
-      size: 1,
-      type: 'normal',
-      ru: ['в', 'В'],
-      en: ['d', 'D']
-   },
-   {
-      id: 'KeyF',
-      size: 1,
-      type: 'normal',
-      ru: ['а', 'А'],
-      en: ['f', 'F']
-   }, {
-      id: 'KeyG',
-      size: 1,
-      type: 'normal',
-      ru: ['п', 'П'],
-      en: ['g', 'G']
-   },
-   {
-      id: 'KeyH',
-      size: 1,
-      type: 'normal',
-      ru: ['р', 'Р'],
-      en: ['h', 'H']
-   }, {
-      id: 'KeyJ',
-      size: 1,
-      type: 'normal',
-      ru: ['о', 'О'],
-      en: ['j', 'J']
-   },
-   {
-      id: 'KeyK',
-      size: 1,
-      type: 'normal',
-      ru: ['л', 'Л'],
-      en: ['k', 'K']
-   }, {
-      id: 'KeyL',
-      size: 1,
-      type: 'normal',
-      ru: ['д', 'Д'],
-      en: ['l', 'L']
-   },
-   {
-      id: 'Key',
-      size: 1,
-      type: 'normal',
-      ru: ['ж', 'Ж'],
-      en: [';', ':']
-   },
-   {
-      id: 'Key',
-      size: 1,
-      type: 'normal',
-      ru: ['э', 'Э'],
-      en: ['"', '"']
-   },
-   {
-      id: 'Enter',
-      size: 3,
-      type: 'functional',
-      ru: ['ENTER', 'ENTER'],
-      en: ['ENTER', 'ENTER']
-   },
-   {
-      id: 'ShiftLeft',
-      size: 3,
-      type: 'functional',
-      ru: ['SHIFT', 'SHIFT'],
-      en: ['SHIFT', 'SHIFT']
-   },
-   {
-      id: 'KeyZ',
-      size: 1,
-      type: 'normal',
-      ru: ['я', 'Я'],
-      en: ['z', 'Z']
-   },
-   {
-      id: 'KeyX',
-      size: 1,
-      type: 'normal',
-      ru: ['ч', 'Ч'],
-      en: ['x', 'X']
-   },
-   {
-      id: 'KeyC',
-      size: 1,
-      type: 'normal',
-      ru: ['c', 'C'],
-      en: ['с', 'С']
-   },
-   {
-      id: 'KeyV',
-      size: 1,
-      type: 'normal',
-      ru: ['м', 'М'],
-      en: ['v', 'V']
-   }, {
-      id: 'KeyB',
-      size: 1,
-      type: 'normal',
-      ru: ['и', 'И'],
-      en: ['b', 'B']
-   },
-   {
-      id: 'KeyN',
-      size: 1,
-      type: 'normal',
-      ru: ['т', 'Т'],
-      en: ['n', 'N']
-   }, {
-      id: 'KeyM',
-      size: 1,
-      type: 'normal',
-      ru: ['ь', 'Ь'],
-      en: ['m', 'M']
-   },
-   {
-      id: 'Coma',
-      size: 1,
-      type: 'normal',
-      ru: ['б', 'Б'],
-      en: [',', '<']
-   },
-   {
-      id: 'Period',
-      size: 1,
-      type: 'normal',
-      ru: ['ю', 'Ю'],
-      en: ['.', '>']
-   },
-   {
-      id: 'Slash',
-      size: 1,
-      type: 'normal',
-      ru: ['.', ','],
-      en: ['/', '?']
-   },
-   {
-      id: 'ArrowUp',
-      size: 1,
-      type: 'arrow',
-      ru: ['&#9650;', '&#9650;'],
-      en: ['&#9650;', '&#9650;']
-   },
-   {
-      id: 'ShiftRight',
-      size: 3,
-      type: 'functional',
-      ru: ['Shift', 'Shift'],
-      en: ['Shift', 'Shift']
-   },
-   {
-      id: 'ControlLeft',
-      size: 2,
-      type: 'functional',
-      ru: ['CTRL', 'CTRL'],
-      en: ['CTRL', 'CTRL']
-   },
-   {
-      id: 'MetaLeft',
-      size: 1,
-      type: 'functional',
-      ru: ['WIN', 'WIN'],
-      en: ['WIN', 'WIN']
-   },
-   {
-      id: 'AltLeft',
-      size: 1,
-      type: 'functional',
-      ru: ['ALT', 'ALT'],
-      en: ['ALT', 'ALT']
-   },
-
-   {
-      id: 'Space',
-      size: 9,
-      type: 'arrow',
-      ru: [' ', ' '],
-      en: [' ', ' ']
-   },
-
-   {
-      id: 'AltRight',
-      size: 1,
-      type: 'functional',
-      ru: ['ALT', 'ALT'],
-      en: ['ALT', 'ALT']
-   },
-   {
-      id: 'ArrowLeft',
-      size: 1,
-      type: 'arrow',
-      ru: ['&#9668;', '&#9668;'],
-      en: ['&#9668;', '&#9668;']
-   },
-   {
-      id: 'ArrowDown',
-      size: 1,
-      type: 'arrow',
-      ru: ['&#9660;', '&#9660;'],
-      en: ['&#9660;', '&#9660;']
-   },
-   {
-      id: 'ArrowRight',
-      size: 1,
-      type: 'arrow',
-      ru: ['&#9658;', '&#9658;'],
-      en: ['&#9658;', '&#9658;']
-   },
-   {
-      id: 'ControlRight',
-      size: 1,
-      type: 'functional',
-      ru: ['CTRL', 'CTRL'],
-      en: ['CTRL', 'CTRL']
-   },
-
+   { id: 'Backquote', size: 1, type: 'normal', ru: ['ё', 'Ё'], en: ['`', '~'] },
+   { id: 'Digit1', size: 1, type: 'number', ru: ['1', '!'], en: ['1', '!'] },
+   { id: 'Digit2', size: 1, type: 'number', ru: ['2', '"'], en: ['2', '@'] },
+   { id: 'Digit3', size: 1, type: 'number', ru: ['3', '№'], en: ['3', '#'] },
+   { id: 'Digit4', size: 1, type: 'number', ru: ['4', ';'], en: ['4', '$'] },
+   { id: 'Digit5', size: 1, type: 'number', ru: ['5', '%'], en: ['5', '%'] },
+   { id: 'Digit6', size: 1, type: 'number', ru: ['6', ':'], en: ['6', '^'] },
+   { id: 'Digit7', size: 1, type: 'number', ru: ['7', '?'], en: ['7', '&'] },
+   { id: 'Digit8', size: 1, type: 'number', ru: ['8', '*'], en: ['8', '*'] },
+   { id: 'Digit9', size: 1, type: 'number', ru: ['9', '('], en: ['9', '('] },
+   { id: 'Digit0', size: 1, type: 'number', ru: ['0', ')'], en: ['0', ')'] },
+   { id: 'Minus', size: 1, type: 'number', ru: ['-', '_'], en: ['-', '_'] },
+   { id: 'Equal', size: 1, type: 'number', ru: ['=', '+'], en: ['=', '+'] },
+   { id: 'Backspace', size: 3, type: 'functional', ru: ['Backspace', 'Backspace'], en: ['Backspace', 'Backspace'] },
+   { id: 'Tab', size: 1, type: 'functional', ru: ['Tab', 'Tab'], en: ['Tab', 'Tab'] },
+   { id: 'KeyQ', size: 1, type: 'normal', ru: ['й', 'Й'], en: ['q', 'Q'] },
+   { id: 'KeyW', size: 1, type: 'normal', ru: ['ц', 'Ц'], en: ['w', 'W'] },
+   { id: 'KeyE', size: 1, type: 'normal', ru: ['у', 'У'], en: ['e', 'E'] },
+   { id: 'KeyR', size: 1, type: 'normal', ru: ['к', 'К'], en: ['r', 'R'] },
+   { id: 'KeyT', size: 1, type: 'normal', ru: ['е', 'Е'], en: ['t', 'T'] },
+   { id: 'KeyY', size: 1, type: 'normal', ru: ['н', 'Н'], en: ['y', 'Y'] },
+   { id: 'KeyU', size: 1, type: 'normal', ru: ['г', 'Г'], en: ['u', 'U'] },
+   { id: 'KeyI', size: 1, type: 'normal', ru: ['ш', 'Ш'], en: ['i', 'I'] },
+   { id: 'KeyO', size: 1, type: 'normal', ru: ['щ', 'Щ'], en: ['o', 'O'] },
+   { id: 'KeyP', size: 1, type: 'normal', ru: ['з', 'З'], en: ['p', 'P'] },
+   { id: 'BracketLeft', size: 1, type: 'normal', ru: ['х', 'Х'], en: ['[', '{'] },
+   { id: 'BracketRight', size: 1, type: 'normal', ru: ['ъ', 'Ъ'], en: [']', '}'] },
+   { id: 'Backslash', size: 1, type: 'normal', ru: ['&#92', '&#47;'], en: ['&#92;', '|'] },
+   { id: 'Delete', size: 1, type: 'functional', ru: ['DEL', 'DEL'], en: ['DEL', 'DEL'] },
+   { id: 'CapsLock', size: 3, type: 'functional', ru: ['Caps Lock', 'Caps Lock'], en: ['Caps Lock', 'Caps Lock'] },
+   { id: 'KeyA', size: 1, type: 'normal', ru: ['ф', 'Ф'], en: ['a', 'A'] },
+   { id: 'KeyS', size: 1, type: 'normal', ru: ['ы', 'Ы'], en: ['s', 'S'] },
+   { id: 'KeyD', size: 1, type: 'normal', ru: ['в', 'В'], en: ['d', 'D'] },
+   { id: 'KeyF', size: 1, type: 'normal', ru: ['а', 'А'], en: ['f', 'F'] },
+   { id: 'KeyG', size: 1, type: 'normal', ru: ['п', 'П'], en: ['g', 'G'] },
+   { id: 'KeyH', size: 1, type: 'normal', ru: ['р', 'Р'], en: ['h', 'H'] },
+   { id: 'KeyJ', size: 1, type: 'normal', ru: ['о', 'О'], en: ['j', 'J'] },
+   { id: 'KeyK', size: 1, type: 'normal', ru: ['л', 'Л'], en: ['k', 'K'] },
+   { id: 'KeyL', size: 1, type: 'normal', ru: ['д', 'Д'], en: ['l', 'L'] },
+   { id: 'Key', size: 1, type: 'normal', ru: ['ж', 'Ж'], en: [';', ':'] },
+   { id: 'Key', size: 1, type: 'normal', ru: ['э', 'Э'], en: ['"', '"'] },
+   { id: 'Enter', size: 3, type: 'functional', ru: ['ENTER', 'ENTER'], en: ['ENTER', 'ENTER'] },
+   { id: 'ShiftLeft', size: 3, type: 'functional', ru: ['SHIFT', 'SHIFT'], en: ['SHIFT', 'SHIFT'] },
+   { id: 'KeyZ', size: 1, type: 'normal', ru: ['я', 'Я'], en: ['z', 'Z'] },
+   { id: 'KeyX', size: 1, type: 'normal', ru: ['ч', 'Ч'], en: ['x', 'X'] },
+   { id: 'KeyC', size: 1, type: 'normal', ru: ['c', 'C'], en: ['с', 'С'] },
+   { id: 'KeyV', size: 1, type: 'normal', ru: ['м', 'М'], en: ['v', 'V'] },
+   { id: 'KeyB', size: 1, type: 'normal', ru: ['и', 'И'], en: ['b', 'B'] },
+   { id: 'KeyN', size: 1, type: 'normal', ru: ['т', 'Т'], en: ['n', 'N'] },
+   { id: 'KeyM', size: 1, type: 'normal', ru: ['ь', 'Ь'], en: ['m', 'M'] },
+   { id: 'Coma', size: 1, type: 'normal', ru: ['б', 'Б'], en: [',', '<'] },
+   { id: 'Period', size: 1, type: 'normal', ru: ['ю', 'Ю'], en: ['.', '>'] },
+   { id: 'Slash', size: 1, type: 'normal', ru: ['.', ','], en: ['/', '?'] },
+   { id: 'ArrowUp', size: 1, type: 'arrow', ru: ['&#9650;', '&#9650;'], en: ['&#9650;', '&#9650;'] },
+   { id: 'ShiftRight', size: 3, type: 'functional', ru: ['Shift', 'Shift'], en: ['Shift', 'Shift'] },
+   { id: 'ControlLeft', size: 2, type: 'functional', ru: ['CTRL', 'CTRL'], en: ['CTRL', 'CTRL'] },
+   { id: 'MetaLeft', size: 1, type: 'functional', ru: ['WIN', 'WIN'], en: ['WIN', 'WIN'] },
+   { id: 'AltLeft', size: 1, type: 'functional', ru: ['ALT', 'ALT'], en: ['ALT', 'ALT'] },
+   { id: 'Space', size: 9, type: 'arrow', ru: [' ', ' '], en: [' ', ' '] },
+   { id: 'AltRight', size: 1, type: 'functional', ru: ['ALT', 'ALT'], en: ['ALT', 'ALT'] },
+   { id: 'ArrowLeft', size: 1, type: 'arrow', ru: ['&#9668;', '&#9668;'], en: ['&#9668;', '&#9668;'] },
+   { id: 'ArrowDown', size: 1, type: 'arrow', ru: ['&#9660;', '&#9660;'], en: ['&#9660;', '&#9660;'] },
+   { id: 'ArrowRight', size: 1, type: 'arrow', ru: ['&#9658;', '&#9658;'], en: ['&#9658;', '&#9658;'] },
+   { id: 'ControlRight', size: 1, type: 'functional', ru: ['CTRL', 'CTRL'], en: ['CTRL', 'CTRL'] }
 ]
 /* Class Key */
 class Key {
@@ -478,7 +98,7 @@ class Key {
       }
       if (this.ru) {
          template += `<span class='ru hidden'>`;
-         template += `<span class='allDown'> ${this.ru[0]}</span>`;
+         template += `<span class='allDown'>${this.ru[0]}</span>`;
          template += `<span class='shiftUp hidden'>${this.ru[1]}</span>`;
          if (this.type != 'number') {
             template += `<span class='capsOn hidden'>${this.ru[1]}</span>`
@@ -505,28 +125,31 @@ class Key {
 }
 /* End class Key */
 
-
-
-/*   Start render page */
-function addTextarea() {
-   let textarea = document.createElement('textarea');
-   textarea.classList.add('input__area');
-   //textarea.setAttribute('autofocus', '');
-   document.body.append(textarea);
-}
+/* Start render page */
 
 const renderKeyboardToDom = () => {
-   addTextarea();
+   //addTextarea();
    let keyboardWrapper = document.createElement('div');
+   document.body.append(keyboardWrapper);
    keyboardWrapper.className = ('keyboard-wrapper')
+   let text1 = document.createElement('h2')
+   text1.textContent = 'VIRTUAL KEYBOARD ver. ANDREI KRASKO'
+   keyboardWrapper.append(text1);
+   let textarea = document.createElement('textarea');
+   textarea.classList.add('input__area');
+   keyboardWrapper.append(textarea);
    let keyboard = document.createElement('div');
    keyboard.className = ('keyboard')
-   keyboardWrapper.appendChild(keyboard);
-   document.body.appendChild(keyboardWrapper);
+   keyboardWrapper.append(keyboard);
    generateKeys(data).forEach(key => {
       keyboard.append(key.generateKey())
    })
-
+   let text2 = document.createElement('h3')
+   text2.textContent = 'Переключение языка: CtrlLeft + AltLeft'
+   keyboardWrapper.append(text2);
+   let text3 = document.createElement('h3')
+   text3.textContent = 'Создано в операционной системе Windows'
+   keyboardWrapper.append(text3);
 }
 
 const generateKeys = (data) => {
@@ -543,7 +166,7 @@ window.addEventListener('DOMContentLoade', renderKeyboardToDom())
 /*  Events */
 
 let inputArea = document.querySelector('textarea');
-
+inputArea.focus()
 document.addEventListener('keydown', (event) => onKeyDown(event));
 document.addEventListener('keyup', (event) => onKeyUp(event));
 document.addEventListener('mousedown', (event) => onMouseDown(event));
@@ -613,6 +236,7 @@ function onKeyUp(event) {
 /* Mouse Events */
 function onMouseDown(event) {
    event.preventDefault();
+   inputArea.focus();
    let keyId = '';
    if (event.target.closest('.key') && event.target.closest('.key').tagName == 'BUTTON') {
       keyId = event.target.closest('.key').dataset.id;
@@ -621,12 +245,14 @@ function onMouseDown(event) {
          //console.log('RU EN')
          langTrigger = true;
       } else langTrigger = false;
+      inputArea.focus();
       getClick(keyId);
    }
 }
 
 function onMouseUp(event) {
    event.preventDefault();
+   inputArea.focus();
    let keyId = '';
    if (event.target.closest('.key') && event.target.closest('.key').tagName == 'BUTTON') {
       keyId = event.target.closest('.key').dataset.id;
@@ -706,6 +332,7 @@ function getSimbol(code) {
             symbol = searchSpan.children[i].innerHTML
          }
       }
+      console.log(symbol, symbol.length)
       return symbol;
    } else return '';
 }
@@ -719,7 +346,7 @@ function getClick(code) {
       } else if (code == 'Delete') {
          deleteSymbol();
       } else if (code == 'Tab') {
-         textSymbol('    ');
+         textSymbol('\t');
       } else if (code == 'Backspace') {
          backspaceSymbol();
       } else if ((code == 'ShiftLeft' || code == 'ShiftRight')) {
@@ -740,7 +367,7 @@ function textSymbol(code) {
    let end = inputArea.selectionEnd;
    inputArea.textContent = inputArea.value.substring(0, start) + code + inputArea.value.substring(end);
    inputArea.focus();
-   inputArea.setSelectionRange(start + code.length, start + code.length);
+   inputArea.setSelectionRange(start + 1, start + 1);
 }
 
 function deleteSymbol() {
